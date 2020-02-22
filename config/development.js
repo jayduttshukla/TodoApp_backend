68045c5f97d02ad80b6ccfdb748e29e5c841370c
Jaydutt Shukla <jayduttshukla11@gmail.com>
2020-02-22 12:35:40 +0530

    Changes

--- config/development.js
+++ config/development.js
@@ -1,4 +1,4 @@
 module.exports = {
-  //  dbURL: 'mongodb://localhost/Todo_App'
-  dbURL: 'mongodb+srv://jayduttt27:GuU3tZxh3NYyzzKF@cluster0-wpdik.mongodb.net/test?retryWrites=true&w=majority'
+    dbURL: 'mongodb://localhost/Todo_App'
+  //dbURL: 'mongodb+srv://jayduttt27:GuU3tZxh3NYyzzKF@cluster0-wpdik.mongodb.net/test?retryWrites=true&w=majority'
 }
