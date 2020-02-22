2bd1f6daa3564968202b05cb9bdd027ae1daf11e
Jaydutt Shukla <jayduttshukla11@gmail.com>
2020-02-22 12:16:25 +0530

    Added Localhost origin

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const config = require('./config/' + (process.env.NODE_ENV || 'development'));
 
 // Middlewares
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
     res.header("Access-Control-Allow-Methods", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Credentials', true);
